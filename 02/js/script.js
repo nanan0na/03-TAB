ff506a6a15ce3b50b636fd3a87a2223efafbb978
nana <goldbarrr5@gmail.com>
2023-12-01 10:44:14 +0900

    배경 이미지 적용

--- 02/js/script.js
+++ 02/js/script.js
@@ -12,6 +12,10 @@ $(function () {
   const $tabMenu = $('.tab-menu > li');
   const $tabContent = $('.tab-con-item');
 
+  // 배경 이미지를 배열에 저장
+  const bgArr = ['blackpink01.jpg', 'blackpink02.jpg', 'blackpink03.jpg'];
+  // console.log(bgArr[2]);
+
   // 초기화
   let idx = 0; //전역변수
   tabAction(0);
@@ -21,6 +25,12 @@ $(function () {
     // 메서드 체인
     $tabMenu.removeClass('on').eq(index).addClass('on');
     $tabContent.hide().eq(index).show();
+
+    // 배경이미지 적용
+    $body.css({
+      // background: 'url(img/' + bgArr[index] + ') no-repeat 0 0 / cover',
+      background: `url(img/${bgArr[index]}) no-repeat 0 0 / cover`,
+    });
   }
 
   $tabMenu.on('click', function (e) {
